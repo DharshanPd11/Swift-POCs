edfec0065010492ff8fff1adc74e0b8ffc3106e2
priyadharshan.r <101190088+DharshanPd11@users.noreply.github.com>
2025-07-27 21:23:02 +0530

    Initialize Swift POCs Xcode project

---
+++ Swift POCs/Swift POCs/Swift_POCsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_POCsApp.swift
+//  Swift POCs
+//
+//  Created by Priyadharshan Raja on 27/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_POCsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
